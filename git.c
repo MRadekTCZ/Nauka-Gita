7e550c8d4c3d078cac85a10d50630fce3193aa1a
MRadekTCZ <97914167+MRadekTCZ@users.noreply.github.com>
2023-01-24 17:05:23 +0100

    Pierwszy nowy plik

---
+++ git.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+void Git(int k);
+
+int main()
+{
+    
+    Git(5);
+    return 0;
+}
+void Git(int k)
+{
+    int i =0;
+for(i=0;i<k;i++)
+{
+printf("Zaczynamy nauke Gita");
+}
+}
